339f58fa137ac222d3e5940df4350766a321dabe
Felipe Valentin <felipe.valentin@jusbrasil.com.br>
2025-05-10 20:09:54 -0300

    progress

--- experiments/src/config.py
+++ experiments/src/config.py
@@ -1,37 +1,4 @@
-# Dizer que data de abertura está em avisos de licitação e edital
-# pedir objeto com todos os detalhes
-# remover tipos inexibilidade e dispensa, são avisos de licitação e edital
-# change from número do processo licitatorio to administrativo
-PROMPT = """Você é um assistente especializado na extração de informações de licitações. 
-Sua tarefa é ler um texto de licitação e extrair as seguintes informações **exatamente como aparecem no texto**, retornando um JSON estruturado conforme o modelo abaixo:
-
-**Campos**
-- Tipo Do Documento: Qual o tipo do documento, Exemplo Aviso de Licitação
-- Número Do Processo Administrativo: deve seguir o formato número/ano, como por exemplo "12/2024, quando ausente igual ao número da modalidade".
-- Município: de Santa Catarina onde ocorreu a licitação.
-- Modalidade: da licitação.
-- Formato Da Modalidade (opcional): Presencial ou Eletrônico. Se não informado retorne null.
-- Número Da Modalidade: da licitação. Deve seguir o formato número/ano, como por exemplo "12/2024".
-- Objeto: Extraia **exatamente como descrito no documento** o objeto da licitação, sem resumir, adicionar texto, reescrever ou interpretar.
-- Data De Abertura (opcional): extraia a **data e horário completos** da abertura do processo licitatório no formato ISO 8601 (exemplo: `2024-10-13T10:30`). **NÃO invente uma data.** Se não informado retorne null.
-    * não extrair se não estiver informado que é de abertura ou início de sessão.
-- Site Do Edital (opcional): apenas se o endereço do site estiver no texto, não inferir a partir de email. exemplo www.saolourenco.sc.gov.br. Se não informado retorne null.
-    * não extrair se não estiver informado que é onde pode ser encontrado o edital.
-- Signatário (opcional): Nome da pessoa que assinou o documento. Se não informado retorne null.
-- Cargo Do Signatário (opcional): Cargo da pessoa que assinou o documento. Se não informado retorne null.
-
-**Responda em Json**
-Se não informado retorne null.
-
-**Exemplos**
-*Entrada*
-{EXEMPLO_1}
-
-*Saída*
-{EXEMPLO_1_OUTPUT}
-"""
 OLLAMA_HOST = "https://ollama-dev.ceos.ufsc.br/"
-MODEL_NAME = "llama3.3:70b"
 OPTIONS = {
     "temperature": 0,
     "seed": 42,
